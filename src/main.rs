53d87ba2871297bf1ab4f0fef7b650c6214f5769
jtbaatz <jeffrey.t.baatz@lmco.com>
2017-12-24 21:53:06 -0500

    Added a initial methods to compute image gradients and to begin project out from edge pixels into other potential stroke edge candidates.
    Needs a lot of work and has issues with many warnings that need to be cleaned up as the code is more fleshed out.
    
    Need to start thinking of ways to test this code without proper ground truth

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,67 @@ extern crate imageproc;
 use std::env;
 use std::path::Path;
 use std::fs;
-use image::{open};
+use std::f32;
+use image::{open, GrayImage, ImageBuffer, Luma};
 use imageproc::edges::canny;
+use imageproc::gradients::{vertical_sobel, horizontal_sobel};
+
+pub struct Point{
+    x: u32,
+    y: u32
+}
+
+impl Point {
+    fn distance(&self) -> f32 {
+        let x2: f32 = (self.x as f32).powi(2);
+        let y2: f32 = (self.y as f32).powi(2);
+        let dist: f32 = (x2 + y2).sqrt();
+        dist
+    }
+}
+
+
+// Ray useful for extending from edge pixels.
+// Includes an iterator that can help extend from one edge pixel to another in a loop.
+pub struct Ray {
+    origin: Point,
+    current: Point,
+    max_pt: Point,
+    direction: f32,
+    iteration: i16 // Note for edge detection we should really not use a velocity higher than 2
+}
+
+impl Ray {
+    fn new(x: u32, y: u32, direction: f32, max_x: u32, max_y: u32) -> Ray {
+        Ray {
+            origin: Point{x: x, y: y},
+            max_pt: Point{x: max_x, y: max_y},
+            current: Point{x: x, y: y},
+            direction: direction,
+            iteration: 0
+        }
+    }
+}
+
+impl Iterator for Ray {
+    // Ray traversal mechanism (assuming positional steps of one)
+    type Item = (Point);
+
+    fn next(&mut self) -> Option<Point> {
+        // theta = adj/hyp
+        let x = ((self.iteration as f32) * self.direction.cos()).round() as u32;
+        let y = ((self.iteration as f32) * self.direction.sin()).round() as u32;
+        let mut new_x = x + self.current.x;
+        let mut new_y = y + self.current.y;
+        if new_x < 0 || new_y < 0 {
+            return None
+        }
+        let new_point = Point {x: new_x, y: new_y};
+        self.current = new_point;
+        self.iteration += 1;
+        return Some(Point{x: new_x, y: new_y});
+    }
+}
 
 
 fn main() {
@@ -40,12 +99,55 @@ fn main() {
     let edges = canny(&input_image, 50., 100.);
     let edge_path = output_folder.join("edges.png");
     edges.save(&edge_path).unwrap();
+
+    let grad_directions = gradient_direction(&input_image);
+
 }
 
 
-// Compute gradient image
+// Compute gradient image and return an angle for each pixel
+fn gradient_direction(image: &GrayImage) -> ImageBuffer<Luma<f32>, Vec<f32>>  {
+    let mut out = ImageBuffer::from_pixel(image.width(), image.height(), Luma {data: [0.0]});
+    let gx = horizontal_sobel(image);
+    let gy = vertical_sobel(image);
 
-// Traver edges in canny image
+    for y in 1..out.height() - 1 {
+        for x in 1..out.width() - 1 {
+        let xgrad = gx[(x, y)][0] as f32;
+        let ygrad = gy[(x, y)][0] as f32;
+        // Get the gradient direction and store it in the array
+        let mut angle = (ygrad).atan2(xgrad);
+        assert!((angle <= 2.*f32::consts::PI)  && (angle >= 0.0)); // Just in case we get some weird angles...
+        out[(x, y)][0] = angle as f32;
+        }
+    }
+    out
+}
+
+fn stroke_width_transform(gradients: &ImageBuffer<Luma<f32>, Vec<f32>>, edges: &GrayImage) -> ImageBuffer<Luma<f32>, Vec<f32>> {
+    let mut swt = ImageBuffer::from_pixel(edges.width(), edges.height(), Luma {data:[0.0]});
+    // Iterate over all pixels in gradients/edges
+    // For each pixel, create a ray that starts at the pixel and traverses along the gradient
+    // When another edge pixel is located, compute the stroke width.
+    // Associate original and final pixel both with a stroke width value
+    for x in 0..swt.width() {
+        for y in 0..swt.height() {
+            let current_pixel = swt[(x,y)][0] as f32;
+            // Check if the swt element has already been associated to a width
+            if current_pixel >= 0.0 || edges[(x,y)][0] < 255 {
+                continue;
+            }
+            let direction = gradients[(x,y)][0];
+            let ray = Ray::new(x, y, direction, swt.width(), swt.height());
+            // for element in ray {
+
+            // }
+        }
+    }
+    swt
+}
+
+// Traverse edges in canny image
 
 // Look up the gradient at each non-zero edge pixel
 
